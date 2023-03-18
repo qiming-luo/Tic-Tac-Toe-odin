a51db52d00840aa8cfb674a2206263ee68e94031
luo-qiming <lowuji22@gmail.com>
2023-03-18 13:07:43 +0800

    add some logic

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const gameboard = (() => {
     }
 })();
 
-// game logic
+//game version 1: choose shape --> player game --> decide winner --> restart game
 
 
 // create object to store data
 function playerCreate(name, shape) {  //factory function
     return {
         player: name,
-        shape: shape
+        shape: shape,
+        arr: []
     }
 }
 
@@ -27,7 +28,23 @@ function handleChooseShape(event) {
     let shape = event.target.innerHTML;
     const player = playerCreate('player', shape);
     console.log(player);
+
+    // handle unit click: it must happen after click choose shape, and must not be occupied.
+    // so it could be wrapped in handleChooseShape
+    function handleUnitClick(event) {
+        event.target.innerHTML = player.shape;
+    }
+    const units = document.getElementsByClassName('unit');
+    console.log(units);
+    units[0].addEventListener('click', handleUnitClick);
+    /*
+    forEach(element => {
+        element.addEventListener('click', handleUnitClick);
+    });
+    */
+    
 }
 
 document.querySelector('#x-shape').addEventListener('click', handleChooseShape);
 document.querySelector('#o-shape').addEventListener('click', handleChooseShape);
+
